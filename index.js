c7823734423bf343f2bcc8a621c3acde4c9d2e54
Yixuan Sherly Lin <linyixuansherly@gmail.com>
2023-10-10 19:49:30 -0400

    Update

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById("myButton").addEventListener("click", onClick);
+
+function onClick(){
+    alert("alert!");
+}
+
+async funtion getPosts{
+    const posts = await db.find(posts.user === "sherly");
+}
