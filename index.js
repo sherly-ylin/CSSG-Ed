e13a7ada18d71c1c2fc9bc2e9b522541ffc58129
Yixuan Sherly Lin <linyixuansherly@gmail.com>
2023-10-14 17:04:07 -0400

    Counter

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-document.getElementById("myButton").addEventListener("click", onClick);
+var count = 0;
 
-function onClick(){
-    alert("alert!");
+const alertBtn = document.getElementById("alertBtn");
+const counter = document.getElementById("count");
+const increaseCountBtn = document.getElementById("incCountBtn");
+const decreaseCountBtn = document.getElementById("decCountBtn");
+const resetCounterBtn = document.getElementById("resetCounterBtn")
+
+window.onload = function(){
+    document.getElementById("count").innerHTML = count;
 }
+
+
+
+alertBtn.addEventListener("click", () => {
+    alert("alert!");
+});
+
+increaseCountBtn.addEventListener("click", () => {
+    count++;
+    counter.innerHTML = count;
+});
+
+decreaseCountBtn.addEventListener("click", () => {
+    count--;
+    counter.innerHTML = count;
+});
+
+resetCounterBtn.addEventListener("click", () => {
+    count = 0;
+    counter.innerHTML = count;
+});
+
+
+
+
 
 // async funtion getPosts{
 //     const posts = await db.find(posts.user === "sherly");
